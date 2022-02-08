4bad8731285dd5393b1752323307626b07bec830
Kris Mitina <kristmitina@gmail.com>
2022-02-08 17:23:05 +0100

    Setting up Project and custom color theme

---
+++ Scrypto/Extensions/Color.swift
@@ -0,0 +1,21 @@
+//
+//  Color.swift
+//  Scrypto
+//
+//  Created by DVKSH on 8.02.22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
---
+++ Scrypto/ScryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScryptoApp.swift
+//  Scrypto
+//
+//  Created by DVKSH on 8.02.22.
+//
+
+import SwiftUI
+
+@main
+struct ScryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
