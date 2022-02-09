59552e78f68cdbd0f046ce6d00d6e5f51ac7cd25
Kris Mitina <kristmitina@gmail.com>
2022-02-09 14:15:09 +0100

    Added HomeView with custom NavigationBar

--- Scrypto/ScryptoApp.swift
+++ Scrypto/ScryptoApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct ScryptoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                HomeView()
+                    .navigationBarHidden(true)
+            }
         }
     }
 }
