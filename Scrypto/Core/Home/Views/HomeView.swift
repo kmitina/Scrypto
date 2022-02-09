90cbb9587f8b3dae367886914930d917f512b89e
Kris Mitina <kristmitina@gmail.com>
2022-02-09 14:15:09 +0100

    Added HomeView with custom NavigationBar

---
+++ Scrypto/Core/Home/Views/HomeView.swift
@@ -0,0 +1,54 @@
+//
+//  HomeView.swift
+//  Scrypto
+//
+//  Created by DVKSH on 9.02.22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @State private var showPortfolio: Bool = false
+    
+    var body: some View {
+        ZStack {
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            VStack {
+                HStack {
+                    CircleButtonView(iconName: showPortfolio ? "plus" : "info" )
+                        .animation(.none)
+                    Spacer()
+                    Text(showPortfolio ? "Portfolio" : "Live Prices")
+                        .font(.headline)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.theme.accent)
+                        .animation(.none)
+                    Spacer()
+                    CircleButtonView(iconName: "chevron.right")
+                        .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                        .onTapGesture {
+                            withAnimation(.spring()) {
+                                showPortfolio.toggle()
+                            }
+                        }
+                }
+                .padding(.horizontal)
+                
+                Spacer(minLength: 0)
+            }
+        }
+        
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            HomeView()
+                .navigationBarHidden(true)
+        }
+    }
+}
