03ba07de6e958a030531a4fed164fab9e5949d79
Kris Mitina <kristmitina@gmail.com>
2022-02-18 23:35:45 +0100

    Downloading Coin Images using Combine, MVVM, and a Networking Layer

---
+++ Scrypto/Services/CoinImageService.swift
@@ -0,0 +1,37 @@
+//
+//  CoinImageService.swift
+//  Scrypto
+//
+//  Created by DVKSH on 18.02.22.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+
+class CoinImageService {
+    
+    @Published var image: UIImage? = nil
+    private var imageSubscription: AnyCancellable?
+    private let coin: CoinModel
+    
+    init(coin: CoinModel) {
+        self.coin = coin
+        getCoinImage()
+    }
+    
+    private func getCoinImage() {
+        guard let url = URL(string: coin.image) else { return }
+        
+        imageSubscription = NetworkingManager.download(url: url)
+            .tryMap({ data -> UIImage? in
+                return UIImage(data: data)
+            })
+            .sink(receiveCompletion: NetworkingManager.handleComplition, receiveValue: { [weak self] returnedImage in
+                self?.image = returnedImage
+                self?.imageSubscription?.cancel()
+            })
+    }
+}
+
