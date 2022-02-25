9acd2d1a40efa8155d6b318ed3306472a64684de
Kris Mitina <kristmitina@gmail.com>
2022-02-25 16:23:44 +0100

    Filtering data based on search bar text using Combine

--- Scrypto/Core/Components/Home/ViewModels/HomeViewModel.swift
+++ Scrypto/Core/Components/Home/ViewModels/HomeViewModel.swift
@@ -17,16 +17,34 @@ class HomeViewModel: ObservableObject {
     
     private let dataService = CoinDataService()
     private var cancellables = Set<AnyCancellable>()
-
+    
     init() {
         addSubscribers()
     }
     
     func addSubscribers() {
-        dataService.$allCoins
-            .sink { [weak self] reternedCoins in
-                self?.allCoins = reternedCoins
+        
+        // updates allCoins
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterCoins)
+            .sink { [weak self] returnedCoins in
+                self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
+    }
+    
+    private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+        guard !text.isEmpty else {
+            return coins
+        }
+        
+        let lowercasedText = text.lowercased()
+        return coins.filter { (coin) -> Bool in
+            return coin.name.lowercased().contains(lowercasedText) ||
+            coin.symbol.lowercased().contains(lowercasedText) ||
+            coin.id.lowercased().contains(lowercasedText)
+        }
     }
 }
