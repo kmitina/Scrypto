792dac050c77a4c8aa12cb85d0b9b6e6c45ea861
Kris Mitina <kristmitina@gmail.com>
2022-03-04 18:47:05 +0100

    Create a view to manager current user's portfolio 2

--- Scrypto/Core/Components/Home/Views/PortfolioView.swift
+++ Scrypto/Core/Components/Home/Views/PortfolioView.swift
@@ -9,14 +9,40 @@ import SwiftUI
 
 struct PortfolioView: View {
     
-    @Environment(\.presentationMode) var presentationMode
+    @EnvironmentObject private var vm: HomeViewModel
+    @State private var selectedCoin: CoinModel? = nil
+    @State private var quantityText: String = ""
     
     var body: some View {
         NavigationView {
             ScrollView {
                 VStack(alignment: .leading, spacing: 0) {
-                    Text("Portfolio VIEW!!!")
+                    SearchBarView(searchText: $vm.searchText)
+                    coinLogoList
                     
+                    if selectedCoin != nil {
+                        VStack(spacing: 20) {
+                            HStack {
+                                Text("Current price of \(selectedCoin?.symbol.uppercased() ?? ""):")
+                                Spacer()
+                                Text(selectedCoin?.currentPrice.asCurrencyWith6Decimals() ?? "")
+                            }
+                            Divider()
+                            HStack{
+                                Text("Amount in your portfolio:")
+                                Spacer()
+                                TextField("Ex: 1.4", text: $quantityText)
+                                    .multilineTextAlignment(.trailing)
+                                    .keyboardType(.decimalPad)
+                            }
+                            Divider()
+                            HStack {
+                                Text("Current value: ")
+                                Spacer()
+                                Text("")
+                            }
+                        }
+                    }
                 }
             }
             .navigationTitle("Edit Portfolio")
@@ -31,5 +57,31 @@ struct PortfolioView: View {
 struct PortfolioView_Previews: PreviewProvider {
     static var previews: some View {
         PortfolioView()
+            .environmentObject(dev.homeVM)
+    }
+}
+
+extension PortfolioView {
+    private var coinLogoList: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            LazyHStack(spacing: 10) {
+                ForEach(vm.allCoins) { coin in
+                    CoinLogoView(coin: coin)
+                        .frame(width: 75)
+                        .padding(4)
+                        .onTapGesture {
+                            withAnimation(.easeIn) {
+                                selectedCoin = coin
+                            }
+                        }
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(selectedCoin?.id == coin.id ? Color.theme.green : Color.clear, lineWidth: 1)
+                        )
+                }
+            }
+            .padding(.vertical, 4)
+            .padding(.leading)
+        }
     }
 }
