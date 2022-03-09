034ba8796146dc9b79d346d42b60dae5a64bfff0
Kris Mitina <kristmitina@gmail.com>
2022-03-09 19:12:58 +0100

    Save current user's portfolio to Core Data with MVVM

--- Scrypto/Core/Components/Home/Views/PortfolioView.swift
+++ Scrypto/Core/Components/Home/Views/PortfolioView.swift
@@ -35,6 +35,11 @@ struct PortfolioView: View {
                     trailingNavBarButton
                 }
             })
+            .onChange(of: vm.searchText) { value in
+                if value == "" {
+                    removeSelectedCoin()
+                }
+            }
         }
     }
 }
@@ -50,13 +55,13 @@ extension PortfolioView {
     private var coinLogoList: some View {
         ScrollView(.horizontal, showsIndicators: false) {
             LazyHStack(spacing: 10) {
-                ForEach(vm.allCoins) { coin in
+                ForEach(vm.searchText.isEmpty ? vm.portfolioCoins : vm.allCoins) { coin in
                     CoinLogoView(coin: coin)
                         .frame(width: 75)
                         .padding(4)
                         .onTapGesture {
                             withAnimation(.easeIn) {
-                                selectedCoin = coin
+                                updateSelectedCoin(coin: coin)
                             }
                         }
                         .background(
@@ -68,6 +73,17 @@ extension PortfolioView {
             .frame(height: 120)
             .padding(.leading)
         }
+    }
+    
+    private func updateSelectedCoin(coin: CoinModel) {
+        selectedCoin = coin
+        
+        if let portfolioCoin = vm.portfolioCoins.first ( where: { $0.id == coin.id }),
+            let amount = portfolioCoin.currentHoldings {
+                quantityText = "\(amount)"
+            } else {
+                quantityText = ""
+            }
     }
     
     private func getCurrentValue() -> Double {
@@ -122,9 +138,13 @@ extension PortfolioView {
     }
     
     private func saveButtonPressed() {
-        guard let coin = selectedCoin else { return }
+        guard let coin = selectedCoin,
+        let amount = Double(quantityText)
+        else { return }
         
         // save to portfolio
+        
+        vm.updatePortfolio(coin: coin, amount: amount)
         
         // show checkmark
         
