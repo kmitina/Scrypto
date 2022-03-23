721980ccaa240f3dc4df0a30b287a8e68bf012b2
Kris Mitina <kristmitina@gmail.com>
2022-03-23 17:25:15 +0100

    Add an icon of a crypto to navBar on the DetailView

--- Scrypto/Core/Components/Detail/Views/DetailView.swift
+++ Scrypto/Core/Components/Detail/Views/DetailView.swift
@@ -53,6 +53,11 @@ struct DetailView: View {
             .padding()
         }
         .navigationTitle(vm.coin.name)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                navigationBarTrailingItems
+            }
+        }
     }
 }
 
@@ -65,6 +70,17 @@ struct DetailView_Previews: PreviewProvider {
 }
 
 extension DetailView {
+    
+    private var navigationBarTrailingItems: some View {
+        HStack {
+            Text(vm.coin.symbol.uppercased())
+                .font(.headline)
+            .foregroundColor(Color.theme.secondaryText)
+            CoinImageView(coin: vm.coin)
+                .frame(width: 25, height: 25)
+        }
+    }
+    
     private var overviewTitle: some View {
         Text("Overview")
             .font(.title)
