091b4841b6a1ed35efb82ea9bcf3f64a97ae2d79
Kris Mitina <kristmitina@gmail.com>
2022-03-23 18:39:33 +0100

    Create a custom line chart to display historical price data

---
+++ Scrypto/Core/Components/Detail/Views/ChartView.swift
@@ -0,0 +1,49 @@
+//
+//  ChartView.swift
+//  Scrypto
+//
+//  Created by DVKSH on 23.03.22.
+//
+
+import SwiftUI
+
+struct ChartView: View {
+    
+    let data: [Double]
+    let maxY: Double
+    let minY: Double
+    
+    init(coin: CoinModel) {
+        data = coin.sparklineIn7D?.price ?? []
+        maxY = data.max() ?? 0
+        minY = data.min() ?? 0
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            Path { path in
+                for index in data.indices {
+                    let xPosition = geometry.size.width / CGFloat(data.count) * CGFloat(index + 1)
+                    
+                    let yAxis = maxY - minY
+                    
+                    let yPosition = (1 - CGFloat((data[index] - minY)) / yAxis) * geometry.size.height
+                    
+                    
+                    
+                    if index == 0 {
+                        path.move(to: CGPoint(x: xPosition, y: yPosition))
+                    }
+                    path.addLine(to: CGPoint(x: xPosition, y: yPosition))
+                }
+            }
+            .stroke(Color.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+        }
+    }
+}
+
+struct ChartView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChartView(coin: dev.coin)
+    }
+}
