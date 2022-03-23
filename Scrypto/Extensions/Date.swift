0e0f005968df126fb37aba8c7a1959c11c4870cd
Kris Mitina <kristmitina@gmail.com>
2022-03-23 20:14:54 +0100

    Create an animation for the graph

---
+++ Scrypto/Extensions/Date.swift
@@ -0,0 +1,30 @@
+//
+//  Date.swift
+//  Scrypto
+//
+//  Created by DVKSH on 23.03.22.
+//
+
+import Foundation
+
+extension Date {
+    
+   // "2018-01-13T00:00:00.000Z"
+    
+    init(coinGeckoString: String) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        let date = formatter.date(from: coinGeckoString) ?? Date()
+        self.init(timeInterval: 0, since: date)
+    }
+    
+    private var shortFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        return formatter
+    }
+    
+    func asShortDateString() -> String {
+        return shortFormatter.string(from: self)
+    }
+}
