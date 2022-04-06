8f91d1c90033d4d2395672c9c3d93a33218c9017
Kris Mitina <kristmitina@gmail.com>
2022-04-06 15:14:02 +0200

    Launch Animation

--- Scrypto/Core/Launch/Views/LaunchView.swift
+++ Scrypto/Core/Launch/Views/LaunchView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct LaunchView: View {
     
-    @State private var loadingText: String = "Loading your portfolio..."
+    @State private var loadingText: [String] = "Loading your portfolio...".map{ String($0) }
+    @State private var showLoadingText: Bool = false
+    private let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+    
+    @State private var counter: Int = 0
+    @State private var loops: Int = 0
+    @Binding var showLaunchView: Bool
     
     var body: some View {
         ZStack {
@@ -18,19 +24,46 @@ struct LaunchView: View {
             Image("logo-transparent")
                 .resizable()
                 .frame(width: 100, height: 100)
+            
             ZStack {
-                Text(loadingText)
-                    .font(.headline)
-                    .foregroundColor(Color.launch.accent)
-                    .fontWeight(.heavy)
+                if showLoadingText {
+                    HStack(spacing: 0) {
+                        ForEach(loadingText.indices) { index in
+                            Text(loadingText[index])
+                                .font(.headline)
+                                .foregroundColor(Color.launch.accent)
+                                .fontWeight(.heavy)
+                                .offset(y: counter == index ? -5 : 0)
+                        }
+                    }
+                    .transition(AnyTransition.scale.animation(.easeIn))
+                }
             }
             .offset(y: 70)
+        }
+        .onAppear {
+            showLoadingText.toggle()
+        }
+        .onReceive(timer) { _ in
+            withAnimation(.spring()) {
+                
+                let lastIndex = loadingText.count - 1
+                if counter == lastIndex {
+                    counter = 0
+                    loops += 1
+                    if loops >= 2 {
+                        showLaunchView = false
+                    }
+                } else {
+                    counter += 1
+                }
+            }
         }
     }
 }
 
 struct LaunchView_Previews: PreviewProvider {
     static var previews: some View {
-        LaunchView()
+        LaunchView(showLaunchView: .constant(true))
     }
 }
